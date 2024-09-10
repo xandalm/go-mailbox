5a3571fd9815200ea5e823ace58668f12c9d3c05
Alexandre L Moreira <alexandre_lopees@hotmail.com>
2024-09-10 16:43:14 -0300

    Improving box creation
    Manager and Storage interfaces.
    Impl. of the Manager following test.
    Preventing id duplicity.

--- mailbox.go
+++ mailbox.go
@@ -1,9 +1,54 @@
 package mailbox
 
+import (
+	"errors"
+	"slices"
+)
+
+var (
+	ErrBoxIDDuplicity = errors.New("mailbox: id duplicity")
+)
+
 type Box struct {
 	Id string
 }
 
-func CreateBox(id string) (Box, error) {
-	return Box{id}, nil
+type Manager interface {
+	CreateBox(string) (Box, error)
+}
+
+type Storage interface {
+	Save(Box) error
+	List() ([]string, error)
+}
+
+type manager struct {
+	st  Storage
+	idx []string
+}
+
+func NewManager(s Storage) Manager {
+	l, _ := s.List()
+	m := &manager{s, l}
+	return m
+}
+
+func (m *manager) contains(id string) (bool, int) {
+	pos, ok := slices.BinarySearch(m.idx, id)
+	return ok, pos
+}
+
+func (m *manager) insert(pos int, id string) {
+	m.idx = slices.Insert(m.idx, pos, id)
+}
+
+func (m *manager) CreateBox(id string) (Box, error) {
+	has, pos := m.contains(id)
+	if has {
+		return Box{}, ErrBoxIDDuplicity
+	}
+	b := Box{id}
+	m.st.Save(b)
+	m.insert(pos, id)
+	return b, nil
 }
--- mailbox_test.go
+++ mailbox_test.go
@@ -6,9 +6,12 @@ import (
 	"github.com/xandalm/go-session/testing/assert"
 )
 
-func TestCreateBox(t *testing.T) {
+func TestBoxCreating(t *testing.T) {
 
-	got, err := CreateBox("box_1")
+	storage := &stubStorage{[]Box{}}
+	manager := NewManager(storage)
+
+	got, err := manager.CreateBox("box_1")
 	want := Box{
 		Id: "box_1",
 	}
@@ -16,4 +19,29 @@ func TestCreateBox(t *testing.T) {
 	assert.NoError(t, err)
 
 	assert.Equal(t, got, want)
+
+	assert.NotEmpty(t, storage.collec)
+
+	assert.Equal(t, storage.collec[0], want)
+
+	t.Run("returns error for duplicity", func(t *testing.T) {
+		box, got := manager.CreateBox("box_1")
+		want := ErrBoxIDDuplicity
+
+		assert.Equal(t, box, Box{})
+		assert.Error(t, got, want)
+	})
+}
+
+type stubStorage struct {
+	collec []Box
+}
+
+func (s *stubStorage) Save(b Box) error {
+	s.collec = append(s.collec, b)
+	return nil
+}
+
+func (s *stubStorage) List() ([]string, error) {
+	return nil, nil
 }
